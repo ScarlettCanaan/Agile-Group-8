0f19000950c69bae66c6a11adecb13e126c97c40
John Wang <john29917958@gmail.com>
2016-04-22 21:20:57 +0800

    Completes UI class

--- src/agile8/system/GradeSystem.java
+++ src/agile8/system/GradeSystem.java
@@ -4,4 +4,25 @@ public class GradeSystem {
 	public GradeSystem() {
 		
 	}
+	
+	public String showGrade(String id) {
+		// TODO: Shows the grades of the user with given ID number.
+		return "Grades shown!";
+	}
+	
+	public String showRank(String id) {
+		// TODO: Shows the ranks of the user with given ID number.
+		return "Ranks shown!";
+	}
+	
+	public void updateWeights() {
+		// TODO: Calculate new weights.
+		System.out.println("Weights updated!");
+	}
+	
+	public boolean containsID(String id) {
+		// TODO: Check if input id is contained in the
+		// user list.
+		return true;
+	}
 }
