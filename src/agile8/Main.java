3fd9597ad7682ccd7a64274cca52e608242ae91b
John Wang <john29917958@gmail.com>
2016-04-22 20:04:56 +0800

    Adds Main class

---
+++ src/agile8/Main.java
@@ -0,0 +1,18 @@
+package agile8;
+
+/**
+ * @author john29917958
+ * Entrance of program.
+ */
+public class Main {
+
+	/**
+	 * @param args Program input.
+	 * In this project, the program input will not be used.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
